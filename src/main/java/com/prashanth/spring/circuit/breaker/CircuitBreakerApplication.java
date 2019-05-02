5bc3f47bdf1d062c1b9d1b3aad5bb77046383105
prashanth-g <prashanthcsgce@gmail.com>
2019-05-02 18:12:21 +0530

    Added FailingService

--- src/main/java/com/prashanth/spring/circuit/breaker/CircuitBreakerApplication.java
+++ src/main/java/com/prashanth/spring/circuit/breaker/CircuitBreakerApplication.java
@@ -2,10 +2,24 @@ package com.prashanth.spring.circuit.breaker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.util.Optional;
 
 @SpringBootApplication
 public class CircuitBreakerApplication {
     public static void main(String[] args) {
         SpringApplication.run(CircuitBreakerApplication.class, args);
+    }
+}
+
+@Service
+class FailingService {
+
+    Mono<String> greet(Optional<String> name) {
+        return name.map(str -> Mono.just("Hola " + str))
+                    .orElse(Mono.error(new NullPointerException()));
     }
 }
