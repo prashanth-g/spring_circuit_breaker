8fbf89926552259140aa1931b9e7938e80a3a3b9
prashanth-g <prashanthcsgce@gmail.com>
2019-05-03 21:57:37 +0530

    Added FailingRestController

--- src/main/java/com/prashanth/spring/circuit/breaker/CircuitBreakerApplication.java
+++ src/main/java/com/prashanth/spring/circuit/breaker/CircuitBreakerApplication.java
@@ -1,9 +1,12 @@
 package com.prashanth.spring.circuit.breaker;
 
+import org.reactivestreams.Publisher;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Service;
-import reactor.core.publisher.Flux;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 
 import java.util.Optional;
@@ -15,6 +18,20 @@ public class CircuitBreakerApplication {
     }
 }
 
+@RestController
+class FailingRestController {
+    private final FailingService failingService;
+
+    FailingRestController(FailingService failingService) {
+        this.failingService = failingService;
+    }
+
+    @GetMapping("/greet")
+    Publisher<String> greet(@RequestParam Optional<String> name) {
+        return this.failingService.greet(name);
+    }
+}
+
 @Service
 class FailingService {
 
@@ -23,3 +40,4 @@ class FailingService {
                     .orElse(Mono.error(new NullPointerException()));
     }
 }
+
