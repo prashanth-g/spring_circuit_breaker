f26b93a2489c801f308a12732869f99458ee428c
prashanth-g <prashanthcsgce@gmail.com>
2019-05-04 20:41:43 +0530

    Added circuit breaker configuration

--- src/main/java/com/prashanth/spring/circuit/breaker/CircuitBreakerApplication.java
+++ src/main/java/com/prashanth/spring/circuit/breaker/CircuitBreakerApplication.java
@@ -3,6 +3,8 @@ package com.prashanth.spring.circuit.breaker;
 import org.reactivestreams.Publisher;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.circuitbreaker.commons.ReactiveCircuitBreaker;
+import org.springframework.cloud.circuitbreaker.commons.ReactiveCircuitBreakerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 
 import java.util.Optional;
+import java.util.function.Function;
 
 @SpringBootApplication
 public class CircuitBreakerApplication {
@@ -21,14 +24,23 @@ public class CircuitBreakerApplication {
 @RestController
 class FailingRestController {
     private final FailingService failingService;
+    private final ReactiveCircuitBreaker reactiveCircuitBreaker;
 
-    FailingRestController(FailingService failingService) {
+    FailingRestController(FailingService failingService,
+                          ReactiveCircuitBreakerFactory reactiveCircuitBreakerFactory) {
         this.failingService = failingService;
+        this.reactiveCircuitBreaker = reactiveCircuitBreakerFactory.create("greet");
     }
 
     @GetMapping("/greet")
     Publisher<String> greet(@RequestParam Optional<String> name) {
-        return this.failingService.greet(name);
+        Mono<String> greetMessage = this.failingService.greet(name);
+        return this.reactiveCircuitBreaker.run(greetMessage, new Function<Throwable, Mono<String>>() {
+            @Override
+            public Mono<String> apply(Throwable throwable) {
+                return Mono.just("Hola from failing service!");
+            }
+        });
     }
 }
 
