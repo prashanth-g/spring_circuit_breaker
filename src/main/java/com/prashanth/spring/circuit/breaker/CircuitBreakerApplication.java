b958a00e222be050e541c0133012ab1b874fff72
prashanth-g <prashanthcsgce@gmail.com>
2019-05-05 17:48:33 +0530

    Added ReactiveCircuitBreakerFactory bean

--- src/main/java/com/prashanth/spring/circuit/breaker/CircuitBreakerApplication.java
+++ src/main/java/com/prashanth/spring/circuit/breaker/CircuitBreakerApplication.java
@@ -1,16 +1,22 @@
 package com.prashanth.spring.circuit.breaker;
 
+import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig;
+import io.github.resilience4j.timelimiter.TimeLimiterConfig;
 import org.reactivestreams.Publisher;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.circuitbreaker.commons.ReactiveCircuitBreaker;
 import org.springframework.cloud.circuitbreaker.commons.ReactiveCircuitBreakerFactory;
+import org.springframework.cloud.circuitbreaker.resilience4j.ReactiveResilience4JCircuitBreakerFactory;
+import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JConfigBuilder;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 
+import java.time.Duration;
 import java.util.Optional;
 import java.util.function.Function;
 
@@ -18,6 +24,22 @@ import java.util.function.Function;
 public class CircuitBreakerApplication {
     public static void main(String[] args) {
         SpringApplication.run(CircuitBreakerApplication.class, args);
+    }
+
+    @Bean
+    ReactiveCircuitBreakerFactory reactiveCircuitBreakerFactory() {
+        ReactiveResilience4JCircuitBreakerFactory factory = new ReactiveResilience4JCircuitBreakerFactory();
+        factory.configureDefault(new Function<String, Resilience4JConfigBuilder.Resilience4JCircuitBreakerConfiguration>() {
+            @Override
+            public Resilience4JConfigBuilder.Resilience4JCircuitBreakerConfiguration apply(String s) {
+                return new Resilience4JConfigBuilder(s)
+                        .timeLimiterConfig(TimeLimiterConfig.custom().timeoutDuration(Duration.ofSeconds(3)).build())
+                        .circuitBreakerConfig(CircuitBreakerConfig.ofDefaults())
+                        .build();
+            }
+        });
+
+        return factory;
     }
 }
 
